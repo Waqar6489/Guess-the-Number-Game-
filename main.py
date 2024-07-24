124bcfaec18d266f7d811c31b344bd63cce2dd8f
Waqar Ali <waqarali6489@gmail.com>
2024-07-24 19:06:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''If the player’s guess is higher than the actual number, the program displays “Lower 
+number please”. Similarly, if the user’s guess is too low, the program prints “higher 
+number please” When the user guesses the correct number, the program displays the 
+number of guesses the player used to arrive at the number.'''
+
 import random
 n= random.randint(1,150)
 Gusses=1
